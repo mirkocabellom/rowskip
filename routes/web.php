b90360aeaed2a98a5bd3b405881c0d89f29401b0
mirkocabellom <mirko.cabello@gmail.com>
2023-06-27 16:02:00 -0400

    modify

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,15 @@ Route::view('/usuario','usuario')->name('usuario');
 Route::view('/estacion','estacion')->name('estacion');
 Route::view('/gestor_estacion','gestor_estacion')->name('gestor_estacion');
 
-Route::view('/inicio','inicio')->name('inicio');
+Route::view('inicio','inicio')->name('inicio');
 Route::view('/tv','tv')->name('tv');
 Route::view('/totem','totem')->name('totem');
+
+Route::get('dashboard', [CustomAuthController::class, 'dashboard']); 
+Route::get('login', [CustomAuthController::class, 'index'])->name('login');
+Route::post('custom-login', [CustomAuthController::class, 'customLogin'])->name('login.custom'); 
+Route::get('registration', [CustomAuthController::class, 'registration'])->name('register-user');
+Route::post('custom-registration', [CustomAuthController::class, 'customRegistration'])->name('register.custom'); 
+Route::get('signout', [CustomAuthController::class, 'signOut'])->name('signout');
+
+
